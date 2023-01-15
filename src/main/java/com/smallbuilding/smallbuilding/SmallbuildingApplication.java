798071a364c90a383b92498a30f2e2a690e872be
seymouryuwu <seymouryuwu@gmail.com>
2023-01-16 01:54:03 +1100

    Initial commit
    Setup Spring Boot.

---
+++ src/main/java/com/smallbuilding/smallbuilding/SmallbuildingApplication.java
@@ -0,0 +1,13 @@
+package com.smallbuilding.smallbuilding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallbuildingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallbuildingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smallbuilding/smallbuilding/SmallbuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smallbuilding.smallbuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallbuildingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
