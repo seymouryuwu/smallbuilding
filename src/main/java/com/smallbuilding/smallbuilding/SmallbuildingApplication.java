d842e5de9277edcb38addf82204071f910236017
seymouryuwu <seymouryuwu@gmail.com>
2023-01-16 18:59:12 +1100

    feat: initialize application building.
    Initialize the application building and inject it to IoC.
    All the information is from the task description:
    Initially the building should have 2 apartments, apartment 101 and 102, a Gym and a Library.
    The building should also have a requested temperature of 25.0 degrees.
    Rooms have heating or cooling enabled based on the following logic:
    If the room temperature is below the requested building temperature, heating should be enabled.
    If the room temperature is above the requested building temperature, cooling should be enabled.

--- src/main/java/com/smallbuilding/smallbuilding/SmallbuildingApplication.java
+++ src/main/java/com/smallbuilding/smallbuilding/SmallbuildingApplication.java
@@ -1,7 +1,9 @@
 package com.smallbuilding.smallbuilding;
 
+import com.smallbuilding.smallbuilding.model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SmallbuildingApplication {
@@ -10,4 +12,46 @@ public class SmallbuildingApplication {
 		SpringApplication.run(SmallbuildingApplication.class, args);
 	}
 
+	/**
+	 * Initialize the application building and inject it to IoC.
+	 * All the information is from the task description.
+	 * @return Returns the application Building object.
+	 */
+	@Bean
+	public Building applicationBuilding() {
+		Building building = new Building();
+
+		building.setRequestedTemperature(25.0);
+
+		initializeApartment(building, 101);
+		initializeApartment(building, 102);
+
+		// Because there is no common room id mentioned in the task, so I just assign it by myself.
+		initializeCommonRoom(building, 1, CommonRoomType.Gym);
+		initializeCommonRoom(building, 2, CommonRoomType.Library);
+
+		return building;
+	}
+
+	private void initializeApartment(Building building, int apartmentId) {
+		Apartment apartment = new Apartment();
+		apartment.setId(apartmentId);
+		initializeRoomTemperature(apartment);
+
+		building.getRooms().put(apartmentId, apartment);
+	}
+
+	private void initializeCommonRoom(Building building, int commonRoomId, CommonRoomType type) {
+		CommonRoom commonRoom = new CommonRoom();
+		commonRoom.setId(commonRoomId);
+		commonRoom.setCommonRoomType(type);
+		initializeRoomTemperature(commonRoom);
+
+		building.getRooms().put(commonRoomId, commonRoom);
+	}
+
+	private void initializeRoomTemperature(Room room) {
+		room.setHeatingEnabled(room.getTemperature() < 25.0);
+		room.setCoolingEnabled(room.getTemperature() > 25.0);
+	}
 }
--- src/test/java/com/smallbuilding/smallbuilding/SmallbuildingApplicationTests.java
+++ src/test/java/com/smallbuilding/smallbuilding/SmallbuildingApplicationTests.java
@@ -1,16 +1,18 @@
 package com.smallbuilding.smallbuilding;
 
-import com.smallbuilding.smallbuilding.model.Apartment;
-import com.smallbuilding.smallbuilding.model.Building;
-import com.smallbuilding.smallbuilding.model.CommonRoom;
-import com.smallbuilding.smallbuilding.model.Room;
+import com.smallbuilding.smallbuilding.model.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SmallbuildingApplicationTests {
+	@Autowired
+	Building building;
 
 	@Test
 	void contextLoads() {
@@ -56,4 +58,40 @@ class SmallbuildingApplicationTests {
 		building.addRoom(null);
 		building.removeRoom(0);
 	}
+
+	/**
+	 * Test if the application building is initialized correctly.
+	 * Initially the building should have 2 apartments, apartment 101 and 102, a Gym and a Library.
+	 * The building should also have a requested temperature of 25.0 degrees.
+	 * Rooms have heating or cooling enabled based on the following logic:
+	 * If the room temperature is below the requested building temperature, heating should be enabled.
+	 * If the room temperature is above the requested building temperature, cooling should be enabled.
+	 */
+	@Test
+	void testInitializingApplicationBuilding() {
+		assertEquals(building.getRequestedTemperature(), 25.0,
+				"Initial building requested temperature is incorrect");
+
+		assertTrue(building.getRooms().get(101) instanceof Apartment, "Room 101 is not an Apartment.");
+		assertTrue(building.getRooms().get(102) instanceof Apartment, "Room 102 is not an Apartment.");
+		assertTrue(building.getRooms().get(1) instanceof CommonRoom, "Room 1 is not an CommonRoom.");
+		assertTrue(building.getRooms().get(2) instanceof CommonRoom, "Room 2 is not an CommonRoom.");
+
+		assertSame(((CommonRoom) building.getRooms().get(1)).getCommonRoomType(), CommonRoomType.Gym,
+				"CommonRoom 1 is not a Gym.");
+		assertSame(((CommonRoom) building.getRooms().get(2)).getCommonRoomType(), CommonRoomType.Library,
+				"CommonRoom 2 is not a Library.");
+
+		for (Map.Entry<Integer, Room> entry: building.getRooms().entrySet()) {
+			Room room = entry.getValue();
+			if (room.getTemperature() < 25.0) {
+				assertTrue(room.isHeatingEnabled(),
+						"Room with id " + room.getId() + " has wrong status for heating.");
+			} else if (room.getTemperature() > 25.0) {
+				assertTrue(room.isCoolingEnabled(),
+						"Room with id " + room.getId() + " has wrong status for cooling.");
+			}
+		}
+	}
+
 }
